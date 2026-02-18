7bc3a2e8032692a09c5ac9bab68ec3883dfd62c3
zahid <zahidmaqbool.mail@gmail.com>
2026-02-18 23:40:11 +0500

    api method write

--- lib/api_methods/get_method_screen.dart
+++ lib/api_methods/get_method_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class GetMethodScreen extends StatefulWidget {
   const GetMethodScreen({super.key});
@@ -8,8 +11,38 @@ class GetMethodScreen extends StatefulWidget {
 }
 
 class _GetMethodScreenState extends State<GetMethodScreen> {
+
+  Future<void> getApiMethod () async {
+    Uri uri = Uri.parse('https://dummyjson.com/products');
+    http.Response  response = await http.get(uri);
+    if(response.statusCode == 200){
+      List<Map<String,dynamic>> data = jsonDecode(response.body);
+      debugPrint(data.toString());
+      print('${response.statusCode} is Status Code / Successful Get Data =====================================================');
+    }else{
+      print('${response.statusCode} is Status Code / Error occur =====================================================');
+    }
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getApiMethod();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GET API'),
+      ),
+      body: SafeArea(child: Column(
+        children: [
+
+        ],
+      ),),
+    );
   }
 }
